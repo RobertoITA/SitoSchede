a5191485b853edfef608dce89878f83b5ba7e745
RobertoITA <roberto.italmont@gmail.com>
2024-10-01 03:00:01 +0200

    Aggiornamento automatico notturno

--- SitoSchede/pro_ita/Scheda_base/bat/pulisci_cartelle.bat
+++ SitoSchede/pro_ita/Scheda_base/bat/pulisci_cartelle.bat
@@ -1,5 +1,5 @@
 @echo off
-for /l %%i in (2,1,200) do (
+for /l %%i in (1,1,200) do (
     setlocal enabledelayedexpansion
     set folder=00%%i
     set folder=!folder:~-3!
@@ -12,18 +12,19 @@ for /l %%i in (2,1,200) do (
             set /a filecount+=1
         )
         
-        rem Verifica se ci sono esattamente due file
-        if !filecount! equ 2 (
-            rem Verifica la presenza dei file specifici
-            if exist "!dest!\HTML - SCHEDA.php" if exist "!dest!\SCHEDA.csv" (
-                echo Eliminando i file HTML - SCHEDA.php e SCHEDA.csv nella cartella !dest!
+        rem Verifica se ci sono meno di due file
+        if !filecount! lss 2 (
+            rem Verifica la presenza dei file specifici e li elimina se presenti
+            if exist "!dest!\HTML - SCHEDA.php" (
+                echo Eliminando il file HTML - SCHEDA.php nella cartella !dest!
                 del "!dest!\HTML - SCHEDA.php"
+            )
+            if exist "!dest!\SCHEDA.csv" (
+                echo Eliminando il file SCHEDA.csv nella cartella !dest!
                 del "!dest!\SCHEDA.csv"
-            ) else (
-                echo File richiesti non trovati in !dest!, nessuna operazione.
             )
         ) else (
-            echo Più di due file trovati in !dest!, nessuna operazione.
+            echo Due o più file trovati in !dest!, nessuna operazione.
         )
     ) else (
         echo La cartella !dest! non esiste.
